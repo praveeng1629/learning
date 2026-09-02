89b9fc71790ec95e08901b5a84cc8750f3dde3d6
Praveen <your-gkpravee007@gmail.com>
2026-09-02 12:00:55 +0530

    Initial commit

---
+++ Employee.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Employee
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int employee_age = obj.nextInt();
+        String employee_name = obj.nextLine();
+        System.out.println(employee_age);
+        System.out.println(employee_name);
+
+    }
+}
